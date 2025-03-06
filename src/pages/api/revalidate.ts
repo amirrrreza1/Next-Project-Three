c28b18a198241a50f77cce6e5bafec21dcee0b2e
Amirreza <arazarioun83@gmail.com>
2025-03-06 21:40:43 +0330

    feat(Revalidation): trigger revalidation for the Blog route after successful updates

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -26,6 +26,7 @@ export default async function handler(
       }
 
       res.status(200).json({ message: "Revalidation successful!" });
+      res.revalidate("/Blog");
     } catch (error) {
       console.error("Error during revalidation:", error);
       res.status(500).json({ message: "Internal Server Error" });
